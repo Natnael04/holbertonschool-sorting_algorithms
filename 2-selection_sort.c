dad8b01d015eacfd0a3df7da0428468c929dbacb
Natnael04 <natnaeldeboch@gmail.com>
2023-05-23 03:27:35 -0700

    addind selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,1 +1,32 @@
 #include "sort.h"
+/**
+ *
+ * 
+ * 
+*/
+
+void selection_sort(int *array, size_t size)
+{
+    long unsigned int i, j, min_idx;
+
+    if (array == NULL)
+    {
+        return;
+    }
+
+    for (i = 0; i < size - 1; i++)
+    {
+        min_idx = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min_idx])
+            {
+                int temp = array[j];
+
+				array[j] = array[min_idx];
+				array[min_idx] = temp;
+				print_array(array, size);
+            }
+        }
+    }
+}
