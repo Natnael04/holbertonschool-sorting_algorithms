72200324d5e71e230f0aec78afa3dfc8466a850a
Natnael04 <natnaeldeboch@gmail.com>
2023-05-23 02:06:44 -0700

    betty fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,23 +3,23 @@
 #include <stdlib.h>
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    
-    if (array == NULL)
-    {
-        return;
-    }
-    for (i = 0; i < size - 1; i++)
-    {
-        for (j = 0; j < size - 1; j++ )
-        {
-            if(array[j] > array[j + 1])
-            {
-                int temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                print_array(array, size);
-            }
-        }
-    }
+	size_t i, j;
+
+	if (array == NULL)
+	{
+		return;
+	}
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1; j++ )
+		{
+			if(array[j] > array[j + 1])
+			{
+				int temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
