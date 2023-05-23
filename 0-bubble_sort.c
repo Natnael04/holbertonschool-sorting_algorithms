928be565b6242950f80937c2ba1a740d129ba007
Natnael04 <natnaeldeboch@gmail.com>
2023-05-23 01:45:35 -0700

    modified

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,6 +4,10 @@
 void bubble_sort(int *array, size_t size)
 {
     size_t i, j;
+    if (array == NULL)
+    {
+        return;
+    }
     for (i = 0; i < size - 1; i++)
     {
         for (j = 0; j < size - 1; j++ )
