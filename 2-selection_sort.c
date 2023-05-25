594719e976985850e09508af33f941d82a725a49
Natnael04 <natnaeldeboch@gmail.com>
2023-05-25 01:45:51 -0700

    add task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,11 +9,6 @@ void selection_sort(int *array, size_t size)
 {
     long unsigned int i, j, min_idx;
 
-    if (array == NULL)
-    {
-        return;
-    }
-
     for (i = 0; i < size - 1; i++)
     {
         min_idx = i;
@@ -21,12 +16,16 @@ void selection_sort(int *array, size_t size)
         {
             if (array[j] < array[min_idx])
             {
-                int temp = array[j];
-
-				array[j] = array[min_idx];
-				array[min_idx] = temp;
-				print_array(array, size);
+                min_idx = j;
             }
         }
+        if (min_idx != i)
+        {
+            int temp = array[i];
+
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+			print_array(array, size);
+        }        
     }
 }
